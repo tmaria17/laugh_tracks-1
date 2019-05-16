03ed24e4e5079ce994dbc003b0293365dda0fe9b
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-05-15 21:10:19 -0600

    Add thumbnail to comedians.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190512041156) do
+ActiveRecord::Schema.define(version: 20190516025539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20190512041156) do
     t.string "city"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo_url"
   end
 
   create_table "specials", force: :cascade do |t|
---
+++ spec/features/user_sees_comedian_thumbnail_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe 'as a user visits the comedian index' do
+  it 'displays comedian thumbnail image' do
+    comedian_1 = Comedian.create(name:"Maria Bamford", age: 48, city: "Eagle Rock", photo_url: "https://m.media-amazon.com/images/M/MV5BMTI4MzQzMjQ3M15BMl5BanBnXkFtZTYwMTU1OTAz._V1_UX214_CR0,0,214,317_AL_.jpg")
+    comedian_2 = Comedian.create(name:"Jessica Williams", age: 29, city: "New York City", photo_url: "https://m.media-amazon.com/images/M/MV5BMTgzMTk3NjgyMl5BMl5BanBnXkFtZTcwMjY2NzkyNw@@._V1_UX214_CR0,0,214,317_AL_.jpg" )
+
+    visit comedians_path
+    save_and_open_page
+    expect(page).to have_css("img[src*='https://m.media-amazon.com/images/M/MV5BMTgzMTk3NjgyMl5BMl5BanBnXkFtZTcwMjY2NzkyNw@@._V1_UX214_CR0,0,214,317_AL_.jpg']")
+    expect(page).to have_css("img[src*='https://m.media-amazon.com/images/M/MV5BMTI4MzQzMjQ3M15BMl5BanBnXkFtZTYwMTU1OTAz._V1_UX214_CR0,0,214,317_AL_.jpg']")
+  end
+end
